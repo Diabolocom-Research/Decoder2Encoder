ee36b0a87f607c8be4680b2086a5665288304e12
NICOLAS BZRD <79255399+Nicolas-BZRD@users.noreply.github.com>
2025-08-04 15:54:33 +0200

    Update model loading condition for Qwen3 to check for substring in huggingface_id

--- optimus/trainer/model/load.py
+++ optimus/trainer/model/load.py
@@ -71,7 +71,7 @@ def load_model(config: Config):
     """
     if config.model.huggingface_id:
         logging.set_verbosity_error()
-        if config.model.huggingface_id.startswith("Qwen/Qwen3"):
+        if "Qwen3" in config.model.huggingface_id:
             from optimus.trainer.model.encoder.dec2enc.biqwen import Qwen3ForMaskedLM
             model = Qwen3ForMaskedLM.from_pretrained(
                 config.model.huggingface_id, return_dict=False, trust_remote_code=True
